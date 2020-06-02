e860d6c770f89788d6ebcbe97811f895d29e6145
Andreas <andreas@jagelund.se>
2020-06-02 16:36:26 +0200

    Add master install script

---
+++ win/box-install.ps1
@@ -0,0 +1,3 @@
+cinst boxstarter -y
+
+Install-BoxstarterPackage -PackageName https://gist.githubusercontent.com/ajagelund/748790e1a695e8cb7975422f675af137/raw/461d73c7527f0e215be501e7c784e92717ae7a59/boxstarter-script.txt -DisableReboots
